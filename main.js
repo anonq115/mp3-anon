1e75381e85cf5e29df63c668eef4b4ed6657333c
lalo salamanca <adampigeons@gmail.com>
2024-05-02 14:54:35 -0700

    Your commit message

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const { app, BrowserWindow, dialog, ipcMain, globalShortcut } = require('electro
 const fs = require('fs');
 const path = require('path');
 
-function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width:410,
+let mainWindow;
+let stickyBoxWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: 410,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
@@ -12,30 +15,78 @@ function createWindow() {
       enableRemoteModule: true,
       preload: path.join(__dirname, 'preload.js')
     },
-    transparent: true, // Set window background to transparent
+    transparent: true,
     backgroundColor: '#00000000',
+    alwaysOnTop: true,
   });
 
   mainWindow.loadFile('index.html');
 
   return mainWindow;
 }
+
+function createStickyBoxWindow() {
+  stickyBoxWindow = new BrowserWindow({
+    width: 300,
+    height: 500,
+    x: 1620,
+    y: 300,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    skipTaskbar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
+  stickyBoxWindow.loadFile(path.join(__dirname, 'sticky-box.html'));
+
+  stickyBoxWindow.setIgnoreMouseEvents(true);
+  stickyBoxWindow.setAlwaysOnTop(true, 'screen-saver', 1); // Set as always on top
+  stickyBoxWindow.setAlwaysOnTop(true, 'pop-up-menu');
+  return stickyBoxWindow;
+}
+
 ipcMain.on('ping', (event, message) => {
   console.log('Received message:', message);
   event.reply('pong', 'Hello from the main process!');
 });
-app.whenReady().then(() => {
-  const mainWindow = createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+app.whenReady().then(() => {
+  mainWindow = createMainWindow();
+  stickyBoxWindow = createStickyBoxWindow();
+
+  globalShortcut.register('F24', () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
     }
   });
 
+  mainWindow.on('blur', () => {
+    if (!stickyBoxWindow.isFocused()) {
+    
+    }
+  });
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+mainWindow.on('close', () => {
+  if (stickyBoxWindow) {
+    stickyBoxWindow.close();
+  }
+});
   ipcMain.on('open-folder-dialog', async (event) => {
     const result = await dialog.showOpenDialog(mainWindow, {
-      properties: ['openDirectory']
+      properties: ['openDirectory'],
+      modal: false // Add this line to prevent the dialog from hiding the main window
+
     });
 
     if (!result.canceled && result.filePaths.length > 0) {
@@ -51,32 +102,13 @@ app.whenReady().then(() => {
       }
 
       const mp3Files = files.filter((file) => file.endsWith('.mp3'))
-                             .map((file) => ({ name: file, path: path.join(folderPath, file) }));
-      
+        .map((file) => ({ name: file, path: path.join(folderPath, file) }));
+
       event.reply('files-list', mp3Files);
     });
   });
 });
 
-
-app.whenReady().then(() => {
-  // ... (Your window creation code)
-
-  // Register the global shortcut for F24
-  globalShortcut.register('F24', () => {
-      ipcRenderer.send('toggle-visibility'); 
-  });
-}); 
-ipcMain.on('toggle-visibility', (event) => {
-  let win = BrowserWindow.getFocusedWindow(); // Or BrowserWindow.getAllWindows()...
-  if (win) { // Check if there's a window to toggle
-      if (win.isVisible()) {
-          win.hide();
-      } else {
-          win.show();
-      }
-  }
-});
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
